11c21d402c52d6121590d3b635f048fe0f4ec0b1
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-23 19:47:45 +0700

    add .gitignore, remove Pods folder, fixbug crash on iPhone 8

--- Pods/Target Support Files/Pods-QR Blank/Pods-QR Blank-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QR_Blank : NSObject
-@end
-@implementation PodsDummy_Pods_QR_Blank
-@end
--- Pods/Target Support Files/Pods-QR Blank/Pods-QR Blank-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_QR_BlankVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_QR_BlankVersionString[];
-
